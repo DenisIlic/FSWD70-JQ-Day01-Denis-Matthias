4734c6c4971694e7b8866fa7d6a877f45d0386fe
Denis Ilic <ilic.denis@gmx.at>
2019-06-03 13:43:21 +0200

    Done

---
+++ Advanced/myscript.js
@@ -0,0 +1,31 @@
+// Change the href attribute to "www.codefactory.wien" for all links
+// $("a").attr("href", "https://codefactory.wien/");
+
+// Change the text color to red for the <code> tags
+// $("code").css("color", "red");
+
+// Change the background color to "#2a7b90" for all <li> elements which are direct child of an <ol> element
+// $("ol li").css("background-color", "#2a7b90");
+
+// Within the form, change the placeholder text to "Express your opinion"
+// $("form textarea").attr("placeholder", "Express your opinion");
+
+// Replace all images on the page with this one "https://www.codefactory.wien/wp-content/uploads/2018/03/codefactory-vienna-footer-h.png"
+// $("img").attr("src", "https://codefactory.wien/wp-content/uploads/2019/05/Logo-new.png")
+
+// Change the background color to orange for all blockquote elements and the font-style to italic
+// $("blockquote").css({"background-color": "orange", "font-style": "italic"});
+
+// At the top of the page, change the logo color "Simple" to black and "Blog" to white
+// $("#logo").css("color", "black");
+// $(".gray").css("color", "white");
+
+// Remove the "Oct 01, 2006" Date from the page
+// $(".date").remove();
+
+// Extend the Sidebar Menu with 3 additional items "New Templates", "Order Template" and "Contact Us"
+// $(".sidemenu").append("<li>New Template</li><li>Order Template</li><li>Contact us</li>");
+
+// Change the sentence "Be not afraid of life. Believe that life is worth living, and your belief will help create the fact." into "When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop."
+// $("#sidebar p:first").text("When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop.")
+
--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -1,8 +1,21 @@
 $("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
-	
-
 });
+
+// Remove the form from the page
+// $("form").remove();
+
+// Create 5 new DIVs in the article element
+// $("article").append("<div>1</<div<div>2</<div<div>3</<div<div>4</<div<div>5</<div");
+
+// Within the input field change the value to "Search for..."
+// $("input").val("Search for...");
+
+// Add a class name of "box" to each new DIV
+// $("div").addClass("box");
+
+// Change the link to "www.codefactory.wien"
+// $("a[href='http://www.codefactory.com/']").attr("href", "https://www.codefactory.wien");
+// $("a").attr("href", "https://www.codefactory.wien");
+
